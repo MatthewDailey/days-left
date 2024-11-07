d8e0d3c8dddccc47588ffedc5389a0d9e519b594
Matt Dailey <mjd@figma.com>
2024-11-06 19:16:12 -0800

    add check fo time left

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // Register a new command that calculates the days left in the year
+  const daysLeftCommand = vscode.commands.registerCommand(
+    "days-left.calculateDaysLeft",
+    () => {
+      const today = new Date();
+      const febSecond = new Date(today.getFullYear() + 1, 1, 2);
+      const oneDay = 1000 * 60 * 60 * 24;
+      const daysLeft = Math.ceil(
+        (febSecond.getTime() - today.getTime()) / oneDay
+      );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "days-left" is now active!');
+      vscode.window.showInformationMessage(`${daysLeft} days left of leave.`);
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('days-left.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from days-left!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(daysLeftCommand);
 }
 
 // This method is called when your extension is deactivated
