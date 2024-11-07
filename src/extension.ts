4abc222326c65b544ead15af0487e7ece7897445
Matt Dailey <mjd@figma.com>
2024-11-07 07:19:42 -0800

    working version with no error

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,38 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
+const getDaysLeft = () => {
+  const today = new Date();
+  const febSecond = new Date(today.getFullYear() + 1, 1, 2);
+  const oneDay = 1000 * 60 * 60 * 24;
+  return Math.ceil((febSecond.getTime() - today.getTime()) / oneDay);
+};
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // Register a new command that calculates the days left in the year
-  const daysLeftCommand = vscode.commands.registerCommand(
-    "days-left.calculateDaysLeft",
-    () => {
-      const today = new Date();
-      const febSecond = new Date(today.getFullYear() + 1, 1, 2);
-      const oneDay = 1000 * 60 * 60 * 24;
-      const daysLeft = Math.ceil(
-        (febSecond.getTime() - today.getTime()) / oneDay
-      );
+  // const daysLeftCommand = vscode.commands.registerCommand(
+  //   "days-left.calculateDaysLeft",
+  //   () => {
+  //     vscode.window.showInformationMessage(
+  //       `${getDaysLeft()} days left of leave.`
+  //     );
+  //   }
+  // );
+  // context.subscriptions.push(daysLeftCommand);
 
-      vscode.window.showInformationMessage(`${daysLeft} days left of leave.`);
-    }
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
   );
-
-  context.subscriptions.push(daysLeftCommand);
+  const updateStatusBar = () => {
+    statusBarItem.text = `${getDaysLeft()} days`;
+  };
+  updateStatusBar();
+  statusBarItem.show();
+  context.subscriptions.push(statusBarItem);
 }
 
 // This method is called when your extension is deactivated
