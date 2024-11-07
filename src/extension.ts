a6fea10de2b3b9e5f42ab7d90c9e8881baec6044
Matt Dailey <mjd@figma.com>
2024-11-07 07:21:00 -0800

    clean up comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 const getDaysLeft = () => {
@@ -9,32 +7,14 @@ const getDaysLeft = () => {
   return Math.ceil((febSecond.getTime() - today.getTime()) / oneDay);
 };
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // Register a new command that calculates the days left in the year
-  // const daysLeftCommand = vscode.commands.registerCommand(
-  //   "days-left.calculateDaysLeft",
-  //   () => {
-  //     vscode.window.showInformationMessage(
-  //       `${getDaysLeft()} days left of leave.`
-  //     );
-  //   }
-  // );
-  // context.subscriptions.push(daysLeftCommand);
-
   const statusBarItem = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Right,
     100
   );
-  const updateStatusBar = () => {
-    statusBarItem.text = `${getDaysLeft()} days`;
-  };
-  updateStatusBar();
+  statusBarItem.text = `${getDaysLeft()} days`;
   statusBarItem.show();
   context.subscriptions.push(statusBarItem);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
