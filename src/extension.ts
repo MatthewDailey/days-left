cc700de9a9d01b14b6cc6b38a0495c4b75bb7310
Matt Dailey <matthew.j.dailey@gmail.com>
2024-12-14 14:52:09 -0800

    bump version

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,15 @@ import * as vscode from "vscode";
 
 const getDaysLeft = () => {
   const today = new Date();
-  const febSecond = new Date(today.getFullYear() + 1, 1, 2);
+  const targetDate = new Date(today.getFullYear(), 0, 19); // Jan 19 of current year
+  
+  // If we're past Jan 19 this year, look to next year
+  if (today > targetDate) {
+    targetDate.setFullYear(today.getFullYear() + 1);
+  }
+  
   const oneDay = 1000 * 60 * 60 * 24;
-  return Math.ceil((febSecond.getTime() - today.getTime()) / oneDay);
+  return Math.ceil((targetDate.getTime() - today.getTime()) / oneDay);
 };
 
 export function activate(context: vscode.ExtensionContext) {
